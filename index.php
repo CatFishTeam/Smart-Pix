8f121008e726cee3d9bf873483022ae204ff179d
Mayon Maël <mael.mayon@free.fr>
2017-03-27 22:00:57 +0200

    mend

--- index.php
+++ index.php
@@ -1,2 +1,82 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bienvenue sur Smart-Pix</title>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link rel="stylesheet" href="/public/css/style.css" />
+        <style>
+            img{
+                max-width: 100%;
+            }
+            .logo {
+                float: left;
+                width: 80px;
+            }
+            .box {
+                border: 1px solid grey;
+                margin: 10px;
+                padding: 5px;
+            }
+            .box h3{
+                
+            }
+        </style>
+    </head>
+    <body class="container">
+        <header>
+            <img src="/public/image/logo.png" alt="Smart-Pix Logo" class="logo"/>
+            <nav>
+                <i class="fa fa-search" aria-hidden="true"></i>
+                <input type="text" placeholder="Recherche par photo, catégorie, artiste..."/>
+                <select>
+                    <option>Connexion / Inscription</option>
+                </select>
+            </nav>
+        </header>
+        <section class="col-4">
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <img src="http://placehold.it/250x120" />
+            <div class="button">
+                <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                Voir plus d'image
+            </div>
+        </section>
+        <aside class="col-2">
+            <div class="box">
+                <h3>Photo du jour</h3>
+                <img src="http://placehold.it/250x120" />
+            </div>
+            <div class="box">
+                <h3>Catégories</h3>
+                <ul>
+                    <li>Nature
+                    <li>Urbain
+                    <li>Animaux
+                    <li>Mode
+                    <li>Arts
+                    <li>Architecture
+                    <li>...
+                </ul>
+            </div>
+        </aside>
+        <footer>
+            Ceci est un footer
+        </footer>
+    </body>
+</html>
