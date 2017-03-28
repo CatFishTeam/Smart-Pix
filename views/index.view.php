22d529bbdcf9e386923673b320ce1581317e09fa
Mayon Maël <mael.mayon@free.fr>
2017-03-28 19:46:41 +0200

    Good for front home

--- views/index.view.php
+++ views/index.view.php
@@ -1,35 +1,6 @@
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Bienvenue sur Smart-Pix</title>
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <link rel="stylesheet" href="public/css/style.css" />
-        <link rel="shortcut icon" type="image/ico" href="/public/image/logo.ico"/>
-    </head>
-    <body>
-        <header>
-            <div class="container">
-                <div class="row">
-                    <section class="col-4">
-                        <img src="public/image/logo.png" alt="Smart-Pix Logo" class="logo"/>
-                        <nav>
-                            <i class="fa fa-search" aria-hidden="true"></i>
-                            <input type="text" placeholder="Recherche par photo, catégorie, artiste..."/>
-                        </nav>
-                    </section>
-                    <section class="col-2">
-                        <select>
-                            <option>Connexion / Inscription</option>
-                        </select>
-                    </section>
-                </div>
-            </div>
-        </header>
-        <div class="container">
+<div class="container">
             <div class="row">
                 <section class="col-4">
-
                     <div id="masonry">
                         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/7.jpg">
                         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/1.jpg">
@@ -66,8 +37,3 @@
                 </aside>
             </div>
         </div>
-        <footer>
-            Smart-Pix © - 2017
-        </footer>
-    </body>
-</html>
