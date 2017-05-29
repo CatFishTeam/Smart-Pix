e5e1df78ca940fce8f5e6bb03830777d22f9c1a9
guillaumepn <guillaumepn@free.fr>
2017-05-30 00:17:24 +0200

    Progress on wall

--- views/frontend.tpl.php
+++ views/frontend.tpl.php
@@ -22,13 +22,13 @@
 
                     <!--    Non connecté :      -->
                     <?php if(!isset($_SESSION['username'])): ?>
-                        <a href="<?php echo PATH_RELATIVE; ?>user/login" class="btn-login">Connexion</a>
-                        <a href="<?php echo PATH_RELATIVE; ?>user/signup" class="btn-signup">Inscription</a>
+                        <a href="<?php echo PATH_RELATIVE; ?>user/login" class="btn btn-login">Connexion</a>
+                        <a href="<?php echo PATH_RELATIVE; ?>user/signup" class="btn btn-signup">Inscription</a>
                     <!--    Connecté :          -->
                     <?php else: ?>
-                        <a href="<?php echo PATH_RELATIVE; ?>user/wall" class="btn-login"><i class="fa fa-camera-retro" aria-hidden="true"></i> My wall</a>
-                        <a href="<?php echo PATH_RELATIVE; ?>user" class="btn-login"><i class="fa fa-user" aria-hidden="true"></i> <?php echo $_SESSION['username']; ?></a>
-                        <a href="<?php echo PATH_RELATIVE; ?>user/logout" class="btn-login"><i class="fa fa-sign-out" aria-hidden="true"></i></a></p>
+                        <a href="<?php echo PATH_RELATIVE; ?>user/wall" class="btn btn-login"><i class="fa fa-camera-retro" aria-hidden="true"></i> My wall</a>
+                        <a href="<?php echo PATH_RELATIVE; ?>user" class="btn btn-login"><i class="fa fa-user" aria-hidden="true"></i> <?php echo $_SESSION['username']; ?></a>
+                        <a href="<?php echo PATH_RELATIVE; ?>user/logout" class="btn"><i class="fa fa-sign-out" aria-hidden="true"></i></a></p>
                     <?php endif; ?>
                 </div>
             </div>
